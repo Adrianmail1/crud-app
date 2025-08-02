d316929ed212956937911943cd4bc4a80937d937
Cojocaru Adrian <adrianmail1@gmail.com>
2025-08-02 11:55:58 +0300

    Add commandLine Runner

--- src/main/java/pixel/academy/crud_app/CrudAppApplication.java
+++ src/main/java/pixel/academy/crud_app/CrudAppApplication.java
@@ -1,13 +1,26 @@
 package pixel.academy.crud_app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudAppApplication {
 
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 		SpringApplication.run(CrudAppApplication.class, args);
-	}
+	    }
 
+
+
+     @Bean
+     public CommandLineRunner commandLineRunner( git status
+	 ) {
+
+		 return runner -> {
+			 System.out.println("Hello world");
+		 };
+
+	 }
 }
